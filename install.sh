e65bb0d2573e5c96ee32096917db23b010c2813e
Niko Honu <nikohonu@gmail.com>
2023-09-20 19:34:28 +0300

    2023-09-20 19:34:28

--- install.sh
+++ install.sh
@@ -6,14 +6,19 @@ install() {
 	path_out=$HOME/$1
 	parent_path=$(dirname "$path_out")
 	echo "ln -sf $path_in $path_out"
-    rm -rf "$path_out"
+	rm -rf "$path_out"
 	mkdir -p $parent_path
 	ln -s "$path_in" "$path_out"
 }
 
+# bash
 install .bashrc
 install .bash_profile
+
+# neovim
 install .config/nvim/init.lua
 install .config/nvim/lua
+
+# bspwm
 install .config/bspwm
 install .config/sxhkd
