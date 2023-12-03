754fd64d3c85c7546f7ffc5c675ffa59179b5361
Niko Honu <nikohonu@gmail.com>
2023-12-03 08:29:28 +0200

    2023-12-03 08:29:23

--- install.sh
+++ install.sh
@@ -11,45 +11,5 @@ install() {
 	ln -s "$path_in" "$path_out"
 }
 
-# yay
-install .config/yay
-
-# scripts
-install scripts
-
-# emacs
-install .config/emacs/init.el
-
-# xdg-user-dir
-install .config/user-dirs.dirs
-
 # bash
 install .bashrc
-install .bash_profile
-
-# neovim
-install .config/nvim/init.lua
-install .config/nvim/lua
-install .vsnip
-
-# hyprland
-install .config/hypr
-
-# bars
-install .config/waybar
-install .config/polybar
-
-# mpv
-install .config/mpv/input.conf
-install .config/mpv/mpv.conf
-install .config/mpv/script-opts
-
-# font
-install .config/fontconfig
-
-# autostart
-install .config/autostart
-
-# bspwm
-install .config/bspwm
-install .config/sxhkd
