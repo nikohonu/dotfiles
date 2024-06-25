ab0fbc0c0a83ffb20bc08fb7cd958858b21d9320
nikohonu <nikohonu@proton.me>
2024-06-25 11:17:48 +0300

    2024-06-25 11:17:48

--- base/.config/emacs/init.el
+++ base/.config/emacs/init.el
@@ -19,6 +19,11 @@
 (use-package org-roam
   :init
   (setq org-roam-directory (file-truename "~/documents/org/"))
+  (setq org-roam-mode-sections
+	(list #'org-roam-backlinks-section
+	      #'org-roam-reflinks-section
+	      #'org-roam-unlinked-references-section
+            ))
   :config
   (org-roam-db-autosync-mode)
   :straight t)
@@ -38,8 +43,20 @@
   (evil-collection-init)
   :straight t)
 
+(use-package org-download 
+  :straight t)
+
+(use-package org-roam-ui
+  :straight t)
+
+(global-set-key (kbd "<f5>") 'redraw-display)
+
 (evil-set-leader nil (kbd "C-SPC"))
 (evil-set-leader 'normal (kbd "SPC"))
 (evil-define-key 'normal 'global (kbd "<leader>pv") 'dired)
 (evil-define-key 'normal 'global (kbd "<leader>rf") 'org-roam-node-find)
+(evil-define-key 'normal 'global (kbd "<leader>dd") 'org-roam-dailies-find-date)
+(evil-define-key 'normal 'global (kbd "<leader>ri") 'org-roam-node-insert)
 (evil-define-key 'normal 'global (kbd "<leader>oa") 'org-agenda)
+
+(global-visual-line-mode t)
