9de9db95b1eb75d20aa8a7839bf81fbe47f04e6d
nikohonu <nikohonu@proton.me>
2024-11-18 12:31:22 +0200

    exclude hardware specific packages

--- pacman.sh
+++ pacman.sh
@@ -6,3 +6,5 @@ sed -i '/nvidia/d' packages.txt
 sed -i '/cuda/d' packages.txt
 sed -i '/xf86-video-amdgpu/d' packages.txt
 sed -i '/xf86-video-ati/d' packages.txt
+sed -i '/lib32-vulkan-radeon/d' packages.txt
+sed -i '/vulkan-radeon/d' packages.txt
